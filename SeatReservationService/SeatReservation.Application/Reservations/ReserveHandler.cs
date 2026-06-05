358b0849add2afe66f8681db38e1d82b46881bf0
Магомедали Гаджиев <mag198421@gmail.com>
2026-06-05 16:50:30 +0300

    feat: добавить создание бронирования с валидацией
    Добавлен контракт ReserveRequest для передачи данных о бронировании. В доменной сущности Reservation реализован статический метод Create с валидацией входных параметров (ID события, пользователя и мест). Создан обработчик ReserveHandler и контроллер ReservationsController для обработки HTTP-запроса. Изменение необходимо для обеспечения целостности данных при создании бронирования.

---
+++ SeatReservationService/SeatReservation.Application/Reservations/ReserveHandler.cs
@@ -0,0 +1,18 @@
+﻿using CSharpFunctionalExtensions;
+using SeatReservation.Contracts;
+using SharedKernel;
+
+namespace SeatReservation.Application.Reservations;
+
+public class ReserveHandler
+{
+    public async Task<Result<Guid, Error>> Handle(ReserveRequest request, CancellationToken cancellationToken)
+    {
+        // Бронирование мест на мероприятии
+        
+        // Создать Reservation с ReservedSeats
+        
+        //
+        
+    }
+}
